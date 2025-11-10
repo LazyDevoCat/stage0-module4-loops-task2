591f34f8f2805eddf4e4152f33bce715438e7a4a
Maria Maksimova <drogzed@gmail.com>
2025-11-10 21:34:10 +0100

    fix prime

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -3,12 +3,13 @@ package school.mjc.stage0.loops.task2;
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
         int counter = 2;
+
         while (counter <= printToInclusive) {
             boolean isPrime = true;
             int divisor = 2;
             while (divisor < counter) {
                 if (counter % divisor == 0) {
-                    System.out.println(counter);
+                    isPrime = false;
                 }
                 divisor++;
             }
